3516e5ace0e55314e7878fc4fd30a6a90e6a729c
Danny12244 <pradeep.py725@gmail.com>
2022-05-24 23:28:09 +0530

    done

--- effects/src/App.js
+++ effects/src/App.js
@@ -1,48 +1,64 @@
-
-import './App.css';
-import React,{ useState ,useEffect } from "react"
-
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-const [newtodo,setnewTodo]=useState([])
-const [todo,setTodo]=useState([])
-const [page,setPage]=useState(1)
-const saveInfo=(page=1 )=>{
-  fetch('http://localhost:3004/todos',{
-    method: 'POST',
-    headers:{
-      "Content-type": "application/json"
-    },
-    body: JSON.stringify({
-    text: newtodo , iscomplete: false
-    }),
-  }).then((r)=>r.json()).then((d)=>{
-    setnewTodo("")
-    setTodo([...todo,d])
-    // console.log(d);
-  });
-} 
+  const [newtodo, setnewTodo] = useState([]);
+  const [todo, setTodo] = useState([]);
+  const [page, setPage] = useState(1);
+  const saveInfo = (page = 1) => {
+    fetch("http://localhost:3004/todos", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        text: newtodo,
+        iscomplete: false,
+      }),
+    })
+      .then((r) => r.json())
+      .then((d) => {
+        setnewTodo("");
+        setTodo([...todo, d]);
+        // console.log(d);
+      });
+  };
 
-useEffect(()=>{
-  fetch(`http://localhost:3004/todos?_page=${page}&_limit=2`).then((r)=>r.json()).then((d)=>{
-    setTodo(d)
-
-  })
-},[page])
+  useEffect(() => {
+    fetch(`http://localhost:3004/todos?_page=${page}&_limit=2`)
+      .then((r) => r.json())
+      .then((d) => {
+        setTodo(d);
+      });
+  }, [page]);
   return (
     <div className="App">
-      <input value={newtodo} onChange={(event)=>{
-  
-        setnewTodo(event.target.value)
-      }}/>
+      <input
+        value={newtodo}
+        onChange={(event) => {
+          setnewTodo(event.target.value);
+        }}
+      />
       <button onClick={saveInfo}>Save</button>
-      {todo.map((el)=>(
+      {todo.map((el) => (
         <div key={el.id}>{el.text}</div>
       ))}
       <h4>Page:{page}</h4>
-      <button disabled={page==1} onClick={()=>{setPage(page-1)}}>Prev</button>
-      <button onClick={()=>{setPage(page+1)}}>Next</button>
+      <button
+        disabled={page == 1}
+        onClick={() => {
+          setPage(page - 1);
+        }}
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 }
